db2f8a1d6468e42cf864ef44759dca17ff128572
vikki200625 <botb737377@gmail.com>
2025-11-01 17:55:36 +0530

    problem fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// In vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/purrfect-number/', // <-- THIS IS THE MAGIC LINE!
 })
